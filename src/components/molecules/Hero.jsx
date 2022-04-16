f97fdae58208a61e796594cd386d403042e5d6a0
Nicolai Larsen <nicolailarsen2100@gmail.com>
2022-04-16 13:17:48 +0200

    added react router

--- src/components/molecules/Hero.jsx
+++ src/components/molecules/Hero.jsx
@@ -5,98 +5,114 @@ import { css } from "@emotion/react";
 import frontImg from "../../images/pexels-desk.jpg";
 
 const style = css`
-   
-*{
-  margin: 0px;
-  padding: 0px;
-  box-sizing: border-box;
-}
+  * {
+    margin: 0px;
+    padding: 0px;
+    box-sizing: border-box;
+    font-family: "Rubik", sans-serif;
+  }
 
-p, h1, h2, h3, h4 {
-  font-family: 'Rubik', sans-serif;
-}
+  .container {
+    margin: 0px auto;
+    width: 1200px;
+  }
 
-.head{
-  background: linear-gradient(rgb(0,0,0), #fff);
-  z-index: -2;
-}
-.container {
-      margin: 0px auto;
-      width: 1200px;
-    }
+  header {
+    height: 100vh;
+    position: relative;
+    z-index: -1;
+    transform: translate(0%, -5%);
+    color: #fff;
+  }
 
-    header {
-      height: 100vh;
-      position: relative;
-      color: #fff;
+  .header-container {
+    width: 1200px;
+    position: absolute;
+    /*in relation to Parent size*/
+    left: 50%;
+    top: 50%;
+    /*in relation to ELEMENT size*/
+    transform: translate(-50%, -50%);
+  }
 
-      z-index: -1;
-      transform: translate(0%, -5%);
-    }
+  .header-container-inner {
+    width: 50%;
+  }
 
-    .header-container {
-      width: 1200px;
-      position: absolute;
-      /*in relation to Parent size*/
-      left: 50%;
-      top: 50%;
-      /*in relation to ELEMENT size*/
-      transform: translate(-50%, -50%);
-    }
+  h1 {
+    font-size: 52px;
+    margin-bottom: 32px;
+  }
 
-    .header-container-inner {
-      width: 50%;
-    }
+  h2 {
+    font-size: 44px;
+    margin-bottom: 48px;
+  }
 
-    h1 {
-      font-size: 52px;
-      margin-bottom: 32px;
-    }
-    
-    h2 {
-      font-size: 44px;
-      margin-bottom: 48px;
-    }
+  p {
+    font-size: 20px;
+    line-height: 1.6;
+    margin-bottom: 48px;
+  }
 
-    p {
-      font-size: 20px;
-      line-height: 1.6;
-      margin-bottom: 48px;
-    }
+  section {
+    padding: 96px 0;
+    background-color: #f7f7f7;
+  }
 
-    section {
-      padding: 96px 0;
-      background-color: #f7f7f7;
-    }
-
-    .btn:link,
-    .btn:visited {
-        border-radius: 9px;
-        font-size: 20px;
-        font-weight: 600;
-        background-color: #e67e22;
-        color: #fff;
-        text-decoration: none;
-        display: inline-block;
-        padding: 16px 32px;
+  .btn:link,
+  .btn:visited {
+    border-radius: 9px;
+    font-size: 20px;
+    font-weight: 600;
+    background-color: #e67e22;
+    color: #fff;
+    text-decoration: none;
+    display: inline-block;
+    padding: 16px 32px;
   }
 
   .pickgradient {
-    display:inline-block;
-    background: -moz-linear-gradient(top, rgba(34,34,34,0.6) 0%, rgba(34,34,34,0.6) 100%); /* FF3.6+ */
-    background: -webkit-gradient(linear, left top, left bottom, color-stop(0%,rgba(34,34,34,0.6)), color-stop(100%,rgba(34,34,34,0.6))); /* Chrome,Safari4+ */
-    background: -o-linear-gradient(top, rgba(34,34,34,0.6) 0%,rgba(34,34,34,0.6) 100%); /* Opera 11.10+ */
-    background: -ms-linear-gradient(top, rgba(34,34,34,0.6) 0%,rgba(34,34,34,0.6) 100%); /* IE10+ */
-    background: linear-gradient(to bottom, rgba(34,34,34,0.6) 0%,rgba(34,34,34,0.6) 100%); /* W3C */
-    background: -webkit-linear-gradient(top, rgba(34,34,34,0.6) 0%,rgba(34,34,34,0.6) 100%); /* Chrome10+,Safari5.1+ */
+    display: inline-block;
+    background: -moz-linear-gradient(
+      top,
+      rgba(34, 34, 34, 0.6) 0%,
+      rgba(34, 34, 34, 0.6) 100%
+    ); /* FF3.6+ */
+    background: -webkit-gradient(
+      linear,
+      left top,
+      left bottom,
+      color-stop(0%, rgba(34, 34, 34, 0.6)),
+      color-stop(100%, rgba(34, 34, 34, 0.6))
+    ); /* Chrome,Safari4+ */
+    background: -o-linear-gradient(
+      top,
+      rgba(34, 34, 34, 0.6) 0%,
+      rgba(34, 34, 34, 0.6) 100%
+    ); /* Opera 11.10+ */
+    background: -ms-linear-gradient(
+      top,
+      rgba(34, 34, 34, 0.6) 0%,
+      rgba(34, 34, 34, 0.6) 100%
+    ); /* IE10+ */
+    background: linear-gradient(
+      to bottom,
+      rgba(34, 34, 34, 0.6) 0%,
+      rgba(34, 34, 34, 0.6) 100%
+    ); /* W3C */
+    background: -webkit-linear-gradient(
+      top,
+      rgba(34, 34, 34, 0.6) 0%,
+      rgba(34, 34, 34, 0.6) 100%
+    ); /* Chrome10+,Safari5.1+ */
     filter: progid:DXImageTransform.Microsoft.gradient( startColorstr='#a6000000', endColorstr='#00000000',GradientType=0 ); /* IE6-9 */
   }
 
-  img{
-    position:relative;
-    z-index:-1;
+  img {
+    position: relative;
+    z-index: -1;
   }
-}
 `;
 
 export default function Hero(props) {
@@ -104,12 +120,9 @@ export default function Hero(props) {
     <div css={style} className="hero-molecule">
       <div css={style}>
         <header>
-          <div
-           className="pickgradient"
-          >
+          <div className="pickgradient">
             <Image image={frontImg} type="hero" alt="A desk full of tools" />
           </div>
-
 
           <div className="header-container">
             <div className="header-container-inner">
